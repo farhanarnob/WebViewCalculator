f37c8b8dd91552dbb9043009a26fbb9acdad0d46
Farhan Rahman Arnob <farhanarnob@live.com>
2016-11-15 15:05:17 +0600

    Some modification for internet connection

--- app/src/main/java/com/example/farha/webviewcalculator/MainActivity.java
+++ app/src/main/java/com/example/farha/webviewcalculator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.farha.webviewcalculator;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.JavascriptInterface;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     WebView webView;
     WebSettings webSettings;
+    ConnectivityManager cm;
     @SuppressLint("JavascriptInterface")
     @Override
     @JavascriptInterface
@@ -18,7 +21,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.web_view);
-        webView.loadUrl("file:///android_asset/local_web_calculator.html");
+        cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() == null) {
+            webView.loadUrl("file:///android_asset/local_web_calculator.html");
+        } else {
+            webView.loadUrl("http://172.16.7.26/android_test/index.html");
+        }
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         WebAppInterface webAppInterface = new WebAppInterface(this);
